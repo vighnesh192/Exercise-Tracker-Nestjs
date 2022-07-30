fbf12f49a57fcc43110d4e36f8a5d89f1e0dd08f
vighnesh192 <vighnesh192000@gmail.com>
2022-07-30 21:29:46 +0530

    from, to and date queries

--- src/exercises/exercises.controller.ts
+++ src/exercises/exercises.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
 import { ExercisesService } from "./exercises.service";
 
 @Controller('/api/users/:_id')
@@ -17,8 +17,41 @@ export class ExercisesController {
     }
 
     @Get('/logs')
-    async getLogs(@Param('_id') userId: string) {
-        const result = await this.ExerciseService.getLogs(userId);
-        return result;
+    async getLogs(
+        @Param('_id') userId: string,
+        @Query('from') from: string,
+        @Query('to') to: string,
+        @Query('limit') limit: number
+    ) {
+        if (!from && !to && !limit) {
+            const result = await this.ExerciseService.getLogs(userId);
+            return result;
+        }
+        else {
+            let result = await this.ExerciseService.getLogs(userId);
+            console.log("LENGTH", result.log.length, limit);
+            console.log("Arr", result.log)
+            if (from || to) {
+                let newLog = result.log.filter((res) => {
+                    if (from) {
+                        if (new Date(res.date) >= new Date(from)) {
+                            if (to) {
+                                if (new Date(res.date) <= new Date(to)) return true;
+                                else return false;
+                            }
+                            else return true;
+                        }
+                    }
+                    else {
+                        if (new Date(res.date) <= new Date(to)) return true;
+                        else false;
+                    }
+                })
+
+                result.log = [...newLog];
+            }
+            if (limit && limit < result.log.length) result.log.length = limit;
+            return result;
+        }
     }
 }
